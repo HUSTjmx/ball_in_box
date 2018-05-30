ef809454fff07a9e4630df7ebcad3bbc36e9f525
jmx <2568094892@qq.com>
2018-05-30 21:01:28 +0800

    to do optimization algorithm

--- ballinbox/ballinbox.py
+++ ballinbox/ballinbox.py
@@ -2,8 +2,11 @@ import math
 import random
 import matplotlib.pyplot as plt
 import ballinbox.validate as vv
-
+E=1e-12
 __all__ = ['ball_in_box']
+
+step1=0.005
+step2=0.0001
 
 def area_sum(circles):
     area = 0.0
@@ -11,6 +14,46 @@ def area_sum(circles):
         area += circle[2]**2 * math.pi
 
     return area
+
+def getmin(aim,circles,blocks):
+    dis=2
+
+    for circle in circles:
+        dis2=math.sqrt((aim[0]-circle[0])**2+(aim[1]-circle[1])**2)-circle[2]
+        if dis>dis2:
+            dis=dis2
+    for block in blocks:
+        dis2 = math.sqrt((aim[0] - block[0]) ** 2 + (aim[1] - block[1]) ** 2)
+        if dis>dis2:
+            dis=dis2
+    for x in [-1,1]:
+        dis2 = math.sqrt((aim[0] - x) ** 2 )
+        if dis>dis2 :
+            dis = dis2
+    for y in [-1,1]:
+        dis2 = math.sqrt((aim[1] -y) ** 2 )
+        if dis>dis2 :
+            dis = dis2
+    return dis
+
+def getarround(raw_circle,circle_index,circles,blocks):
+    x_old=raw_circle[0]
+    y_old=raw_circle[1]
+    x=raw_circle[0]-step1
+    y=raw_circle[1]-step1
+    middle_circle=[(0,0,0)]
+    while x<x_old+step1:
+        while y<y_old+step1:
+            r = getmin((x, y), circles, blocks)
+            if (middle_circle[0][2] < r):
+                middle_circle[0] = (x, y, r)
+            y=y+step2
+        x=x+step2
+        y=raw_circle[1]-step1
+    if middle_circle[0][2]<raw_circle[2]:
+        middle_circle[0]=raw_circle
+    return middle_circle[0]
+
 
 def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
     """
@@ -21,47 +64,45 @@ def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
     """
 
     # The following is an example implementation.
-    print('请输入气球的数量：')
-    m=int(input())
-    print("是否自定应点（1：是，0：否）：")
-    choose=int(input())
+    n=int(input('请输入气球的数量（输入非正数则取默认气球数5）：'))
+    if m>0:
+        m=n
+    choose=int(input("是否自定应点（1：是，0：否）："))
     if choose:
         blockers=[]
         conti=1
         while conti:
-            print('输入点的坐标(形式：x,y)：')
-            n=tuple(input())
-            blockers.append((int(n[0]),int(n[2])))
-            print("是否继续输入(1:是，0：否)：")
-            conti=int(input())
-    print(blockers)
+            n=input('输入点的坐标(形式：x,y)：')
+            xlist = n.split(",")
+            x=float(xlist[0])
+            y=float(xlist[1])
+            blockers.append((x,y))
+            conti=int(input("是否继续输入(1:是，0：否)："))
+    print("障碍点：",blockers)
     circles=[]
     circle_index=0
     raw_circle=[(0,0,0)]
     while circle_index<m:
-        x=-0.98
-        y=-0.98
-        r=0.015
-        circles.append((0,0,0))
+        x=-1
+        y=-1
         while x<1:
             while y<1:
-                while r<1:
-                     circles[circle_index]=(x, y, r)
-                     if  not vv.validate(circles,blockers):
-                         if raw_circle[0][2]<r-0.005:
-                             raw_circle[0]=(x,y,r-0.005)
-                         break
-                     r=r+0.005
-                y=y+0.01
-                r=0.015
-            x=x+0.01
-            y=-0.98
-        circles[circle_index]=raw_circle[0]
+                r=getmin((x,y),circles,blockers)
+                if(raw_circle[0][2]<r):
+                    raw_circle[0]=(x,y,r)
+                y=y+step1
+            x=x+step1
+            y=-0.995
+        raw_circle[0] = getarround(raw_circle[0], circle_index, circles, blockers)
+        circles.append(raw_circle[0])
         print('找到了第',circle_index+1,'个圆',raw_circle)
         raw_circle=[(0,0,0)]
         circle_index=circle_index+1
-    area = area_sum(circles)
-    print("Total area: {}".format(area))
+    if vv.validate(circles,blockers):
+        area = area_sum(circles)
+        print("Total area: {}".format(area))
+    else:
+        print ("error")
     fig=plt.figure()
     ax = fig.add_subplot(111)
     plt.xlim((-1,1))
--- ballinbox/validate.py
+++ ballinbox/validate.py
@@ -3,31 +3,6 @@ import math
 E=1e-12
 
 def validate(circles, blockers):
-    # Is circle in the box?
-    for circle in circles:
-        xmr = circle[0] - circle[2]
-        xpr = circle[0] + circle[2]
-        ymr = circle[1] - circle[2]
-        ypr = circle[1] + circle[2]
-
-        if (not (xmr <= 1 and xmr >=-1 )) \
-           or (not (xpr <= 1 and xpr >=-1 )) \
-           or (not (ymr <= 1 and ymr >=-1 )) \
-           or (not (ypr <= 1 and ypr >=-1 )):
-            return False
-
-    # Is circle good for blockers?
-    if blockers is not None and len(blockers) > 0:
-        for circle in circles:
-            for block in blockers:
-                x = circle[0]
-                y = circle[1]
-                r = circle[2]
-                bx = block[0]
-                by = block[1]
-                if math.sqrt((x - bx)**2 + (y - by)**2) < r:
-                    return False
-
     for circle1 in circles:
         for circle2 in circles:
             if circle1==circle2:
@@ -39,7 +14,31 @@ def validate(circles, blockers):
                 x2 = circle2[0]
                 y2 = circle2[1]
                 r2 = circle2[2]
-                if math.sqrt((x1 - x2)**2 + (y1 - y2)**2)-r1-r2<E:
+                if math.sqrt((x1 - x2)**2 + (y1 - y2)**2)-(r1+r2)<-E:
+                    return False
+    # Is circle in the box?
+    for circle in circles:
+        xmr = circle[0] - circle[2]
+        xpr = circle[0] + circle[2]
+        ymr = circle[1] - circle[2]
+        ypr = circle[1] + circle[2]
+
+        if (not (xmr <= 1+E and xmr >=-1-E )) \
+           or (not (xpr <= 1+E and xpr >=-1-E )) \
+           or (not (ymr <= 1+E and ymr >=-1-E )) \
+           or (not (ypr <= 1+E and ypr >=-1-E )):
+            return False
+
+    # Is circle good for blockers?
+    if blockers is not None and len(blockers) > 0:
+        for circle in circles:
+            for block in blockers:
+                x = circle[0]
+                y = circle[1]
+                r = circle[2]
+                bx = block[0]
+                by = block[1]
+                if math.sqrt((x - bx)**2 + (y - by)**2) -r< -E:
                     return False
     # all good
     return True
